1ed17d13765cb14550592f33fcfb6c6ffe3ec760
anisha <anisha.narang27@gmail.com>
2014-08-06 20:20:01 +0530

    adding code to save screenshot if a scenario fails

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,8 +6,8 @@ require File.expand_path('features/lib/search_methods')
 require 'cucumber/formatter/gherkin_formatter_adapter'
 require 'cucumber/formatter/io'
 require 'gherkin/formatter/json_formatter'
-#require 'rspec-expectations'
-
+require 'json'
+require 'rspec'
 
 headless = Headless.new
 headless.start
@@ -22,6 +22,15 @@ baseURL = CONFIG['url']
 Before do
   @browser = browser
   @url = baseURL
+end
+
+After do |scenario|
+  if scenario.failed?
+    Dir::mkdir('screenshots') if not File.directory?('screenshots')
+    screenshot = "./screenshots/FAILED_#{scenario.name.gsub(' ','_').gsub(/[^0-9A-Za-z_]/, '')}.png"
+    @browser.screenshot.save screenshot
+    embed screenshot, 'image/png'
+  end
 end
 
 at_exit do
